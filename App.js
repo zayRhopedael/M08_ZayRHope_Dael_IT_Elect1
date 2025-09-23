1827c1017b7b18bc7382ab69f38ccbab1904b578
zayRhopedael <zaydael0923@gmail.com>
2025-09-23 22:02:02 +0800

    Initial commit, Main Branch

---
+++ App.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import Messenger from './MidAct1/Messenger';
+import CommentSection from './MidAct1/CommentSection';
+
+export default function App() {
+  return (
+    <>   
+      <Messenger />
+      <CommentSection />
+    </>
+  );
+}
+
